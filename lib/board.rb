2d58d588abc285db589df3601c2cf620170c3e2a
rafaeldx7 <rafaeldx7@gmail.com>
2012-01-23 21:47:37 -0200

    add método que guarda linha atual do robô

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,15 @@
+class BoardRowOutOfBoundsException < Exception; end
+
 class Board
   attr_accessor :rows, :cols
 
   def initialize(rows, cols)
     self.rows = rows
     self.cols = cols
+  end
+
+  def row=(row)
+    raise BoardRowOutOfBoundsException if row > self.rows || row <= 0
+    @row = row
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe Board do
-  describe "initialize" do
+  describe "#initialize" do
     it "set rows and cols" do
       board = Board.new(10, 10)
       board.rows.should == 10
       board.cols.should == 10
+    end
+  end
+
+  describe "#row=" do
+    it "return row" do
+      board = Board.new(1, 1)
+      result = board.row=(1)
+      result.should == 1
+    end
+    it "raise error if new row is 0" do
+      board = Board.new(1, 1)
+      expect { board.row = 0 }.to raise_error(BoardRowOutOfBoundsException)
+    end
+    it "raise error if new row is less than 0" do
+      board = Board.new(1, 1)
+      expect { board.row = -2 }.to raise_error(BoardRowOutOfBoundsException)
+    end
+    it "raise error if new row is greater than rows" do
+      board = Board.new(1, 1)
+      expect { board.row = 2 }.to raise_error(BoardRowOutOfBoundsException)
     end
   end
 end
