e5315ed576d3ec969e48affca31b7a532edec71c
rafaeldx7 <rafaeldx7@gmail.com>
2012-01-23 23:12:29 -0200

    refat: usando o let para reduzir código

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe Board do
   include Orientation
 
+  let(:board) { Board.new(10, 10) }
+
   describe "#initialize" do
     it "set rows and cols" do
-      board = Board.new(10, 10)
       board.rows.should == 10
       board.cols.should == 10
     end
@@ -13,74 +14,60 @@ describe Board do
 
   describe "#row=" do
     it "return row" do
-      board = Board.new(1, 1)
       result = board.row=(1)
       result.should == 1
     end
     it "raise error if new row is 0" do
-      board = Board.new(1, 1)
       expect { board.row = 0 }.to raise_error(BoardRowOutOfBoundsException)
     end
     it "raise error if new row is less than 0" do
-      board = Board.new(1, 1)
       expect { board.row = -2 }.to raise_error(BoardRowOutOfBoundsException)
     end
     it "raise error if new row is greater than rows" do
-      board = Board.new(1, 1)
-      expect { board.row = 2 }.to raise_error(BoardRowOutOfBoundsException)
+      expect { board.row = 11 }.to raise_error(BoardRowOutOfBoundsException)
     end
   end
 
   describe "#col=" do
     it "return col" do
-      board = Board.new(1, 1)
       result = board.col=(1)
       result.should == 1
     end
     it "raise error if new col is 0" do
-      board = Board.new(1, 1)
       expect { board.col = 0 }.to raise_error(BoardColOutOfBoundsException)
     end
     it "raise error if new col is less than 0" do
-      board = Board.new(1, 1)
       expect { board.col = -2 }.to raise_error(BoardColOutOfBoundsException)
     end
     it "raise error if new col is greater than cols" do
-      board = Board.new(1, 1)
-      expect { board.col = 2 }.to raise_error(BoardColOutOfBoundsException)
+      expect { board.col = 11 }.to raise_error(BoardColOutOfBoundsException)
     end
   end
 
   describe "#orientation=" do
     it "set and return 'N' orientation" do
-      board = Board.new(5, 5)
       result = board.orientation=(N)
       result.should == N
     end
     it "set and return 'S' orientation" do
-      board = Board.new(5, 5)
       result = board.orientation=(S)
       result.should == S
     end
     it "set and return 'E' orientation" do
-      board = Board.new(5, 5)
       result = board.orientation=(E)
       result.should == E
     end
     it "set and return 'W' orientation" do
-      board = Board.new(5, 5)
       result = board.orientation=(W)
       result.should == W
     end
     it "raise error when orientation is invalid" do
-      board = Board.new(5, 5)
       expect { board.orientation=('T') }.to raise_error(InvalidOrientationException)
     end
   end
 
   describe "#row" do
     it "return row" do
-      board = Board.new(5, 5)
       board.row = 3
       board.row.should == 3
     end
@@ -88,7 +75,6 @@ describe Board do
 
   describe "#col" do
     it "return col" do
-      board = Board.new(5, 5)
       board.col = 3
       board.col.should == 3
     end
@@ -96,7 +82,6 @@ describe Board do
 
   describe "#orientation" do
     it "return orientation" do
-      board = Board.new(5, 5)
       board.orientation = N
       board.orientation.should == N
     end
