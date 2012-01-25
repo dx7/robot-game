f541511e632508f8026480ee5332891d4abd5f72
rafaeldx7 <rafaeldx7@gmail.com>
2012-01-25 00:02:21 -0200

    add gem simplecov para medir cobertura de testes - quase 100% :)

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source "https://rubygems.org"
 
 gem 'rake', '0.9.2'
 gem 'rspec', '2.8.0'
+gem 'simplecov', '0.5.4', :require => false
 gem 'bundler', '1.1.rc.7'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path("./lib/board")
 require File.expand_path("./lib/robot")
 require File.expand_path("./bin/robot")
